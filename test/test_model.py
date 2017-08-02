a5099646b8e541decd990ecda45cfbc51cb4a24b
Dan Meliza <dan@meliza.org>
2017-08-02 16:04:18 -0400

    add some very simple tests

--- test/test_model.py
+++ test/test_model.py
@@ -7,14 +7,23 @@ import numpy as np
 from mat_neuron import core
 
 dt = 1.0
-params = [10, 2, 0, 5, 10, 10, 10, 200, 5, 2]
 state = [0, 0, 0, 0, 0]
 
-
 def test_step_response():
+    params = [10, 2, 0, 5, 10, 10, 10, 200, 5, 2]
     I = np.zeros(2000, dtype='d')
     I[500:1500] = 0.5
     Y, S = core.predict(state, params, I, dt)
 
     T = np.asarray([531, 945, 1383])
-    assert_true(all((T - S) < 0.001))
+    assert_true(all(np.abs(T - S) < 0.001))
+
+
+def test_phasic_response():
+    params = [10, 2, -0.3, 5, 10, 10, 10, 200, 5, 2]
+    I = np.zeros(2000, dtype='d')
+    I[500:1500] = 0.45
+    Y, S = core.predict(state, params, I, dt)
+
+    assert_equal(len(S), 1)
+    assert_true(np.abs(S[0] - 515) < 0.001)
